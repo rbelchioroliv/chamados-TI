9a1112274e5ffb91168c96520ff95a6b3ff30ded
Renan Belchior de Oliveira <renan.oliveira@pirangi.local>
2025-07-11 10:05:20 -0300

    URL da API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // URL base do nosso back-end que está rodando na porta 3001
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://sistema-chamados-api.onrender.com',
 });
 
 export default api;
