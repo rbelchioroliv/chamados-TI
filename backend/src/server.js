42285b0eacd32ec301428c0304b2ad4fbec8a981
Renan Belchior de Oliveira <renan.oliveira@pirangi.local>
2025-07-11 10:38:19 -0300

    Configuração do CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,23 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
+const allowedOrigins = [
+  'https://chamados-ti.vercel.app', 
+  'http://localhost:5173'           
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acesso não permitido pelo CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.post('/api/register', async (req, res) => {
   const { name, username, email, department, password } = req.body;
