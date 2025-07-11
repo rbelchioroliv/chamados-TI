faf94563934c90781005e5aa1d6249bfb5941a0b
Renan Belchior de Oliveira <renan.oliveira@pirangi.local>
2025-07-11 10:25:46 -0300

    Conexão com a API resolvida

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // URL base do nosso back-end que está rodando na porta 3001
-  baseURL: 'http://localhost:3001/api',
+  baseURL: '${import.meta.env.VITE_API_URL}/api',
 });
 
 export default api;
